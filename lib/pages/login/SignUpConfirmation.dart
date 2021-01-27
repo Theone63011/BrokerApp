e8d24061f49a4b03e6ed8dc9d2822f795737b935
Ed O'Neil <eddieo63011@gmail.com>
2021-01-26 20:42:39 -0500

    Device confirmation [working]

--- lib/pages/login/SignUpConfirmation.dart
+++ lib/pages/login/SignUpConfirmation.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'dart:math';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
@@ -12,7 +13,7 @@ import 'package:flutter_login/flutter_login.dart';
 import 'package:revire/widgets/NumberPad.dart';
 import 'package:amazon_cognito_identity_dart_2/cognito.dart';
 import 'package:amazon_cognito_identity_dart_2/sig_v4.dart';
-
+import 'package:device_info/device_info.dart';
 
 class SignUpConfirmation extends StatefulWidget {
   SignUpConfirmation({Key key}) : super(key: key);
@@ -46,13 +47,99 @@ class SignUpConfirmationState extends State<SignUpConfirmation> {
     bool signedUp = _store.get(Constants.signUpConfirmedKey);
 
     if (loggedIn || signedUp) {
-      log.error("ERROR- The global store indicates that:\n" + "loggedIn = " +
-          loggedIn.toString() + ", and signedUp = " + signedUp.toString() + ".\nThis SHOULD NOT HAPPEN!");
-      Constants.showDialog_CloseOption(context, "Confirmation Error!", "ERROR- The global store indicates that:\n" +
-          "loggedIn = " + loggedIn.toString() + ", and signedUp = " + signedUp.toString() + ".\nThis SHOULD NOT HAPPEN!");
+      log.error("ERROR- The global store indicates that:\n" +
+          "loggedIn = " +
+          loggedIn.toString() +
+          ", and signedUp = " +
+          signedUp.toString() +
+          ".\nThis SHOULD NOT HAPPEN!");
+      Constants.showDialog_CloseOption(
+          context,
+          "Confirmation Error!",
+          "ERROR- The global store indicates that:\n" +
+              "loggedIn = " +
+              loggedIn.toString() +
+              ", and signedUp = " +
+              signedUp.toString() +
+              ".\nThis SHOULD NOT HAPPEN!");
       return true;
     } else {
       return false;
+    }
+  }
+
+  Future<Map<String, String>> getDeviceDetails() async {
+    log.info("getDeviceDetails() called.");
+
+    //TODO: SECURITY WARNING:
+    //    With a rooted phone, some of these values could be incorrect or misrepresented.
+    //    Consider adding additional security here.
+
+    //TODO: When adding web support, this needs to be updated.
+
+    String device;
+    String deviceName;
+    String deviceVersion;
+    String deviceId;
+    final DeviceInfoPlugin deviceInfoPlugin = new DeviceInfoPlugin();
+    try {
+      if (Platform.isAndroid) {
+        AndroidDeviceInfo deviceInfo = await deviceInfoPlugin.androidInfo;
+        device = deviceInfo.device;
+        deviceName = deviceInfo.model;
+        deviceVersion = deviceInfo.version.toString();
+        deviceId = deviceInfo.androidId;
+      } else if (Platform.isIOS) {
+        IosDeviceInfo deviceInfo = await deviceInfoPlugin.iosInfo;
+        device = deviceInfo.name;
+        deviceName = deviceInfo.model;
+        deviceVersion = deviceInfo.systemVersion;
+        deviceId = deviceInfo.identifierForVendor;
+      } else {
+        log.error("ERROR! Platform found is NEITHER Android OR IOS.");
+        return null;
+      }
+    } catch (error) {
+      log.error("ERROR! Could not get device details from 'DeviceInfoPlugin'.");
+      return null;
+    }
+    final Map<String, String> deviceDetails = {
+      "device":device,
+      "deviceName":deviceName,
+      "deviceVersion":deviceVersion,
+      "deviceId":deviceId,
+    };
+    return deviceDetails;
+  }
+
+  Future<bool> confirmDevice(BuildContext context) async {
+    log.info("confirmDevice(BuildContext context) called.");
+    data = _store.get(Constants.loginDataKey);
+    if (data.name == null) {
+      log.error("data.name is null. THIS SHOULD NOT HAPPEN");
+      Constants.showDialog_UnexpectedError(context);
+      Navigator.pop(context);
+    }
+
+    Map<String, String> deviceDetails = await getDeviceDetails();
+    log.info("Device Details: ");
+    deviceDetails.forEach((key, value) {
+      log.info(key + ": " + value);
+    });
+
+    return Future.value(true);
+
+    try {
+      AuthUser user = await Amplify.Auth.getCurrentUser();
+      String userId = user.userId;
+
+
+      //TODO - conintue...
+
+      
+
+    } on AuthError catch (error) {
+
     }
   }
 
@@ -66,13 +153,19 @@ class SignUpConfirmationState extends State<SignUpConfirmation> {
     }
     try {
       SignUpResult res = await Amplify.Auth.confirmSignUp(username: data.name, confirmationCode: enteredCode);
-      if(res.isSignUpComplete) {
+      if (res.isSignUpComplete) {
         log.info("Confirm sign up Successful!");
         _store.set(Constants.signUpConfirmedKey, true);
-        //TODO- continue to home page
-        Constants.showInProgressDialog(context);
-      }
-      else {
+        bool deviceConfirmationResult = await confirmDevice(context);
+        if (deviceConfirmationResult) {
+          log.info("Device confirmation successful.");
+          //TODO- continue to home page
+          Constants.showInProgressDialog(context);
+        } else {
+          log.error("ERROR! Device confirmation FAILED.");
+          //TODO- what happens if the device cannot be confirmed?
+        }
+      } else {
         log.error("Sign up incomplete! result: " + res.toString());
         Constants.showDialog_UnexpectedError(context);
         _store.set(Constants.signUpConfirmedKey, false);
@@ -85,7 +178,7 @@ class SignUpConfirmationState extends State<SignUpConfirmation> {
         log.error("\t" + element.exception + " Exception- " + element.detail.toString());
         if (element.exception == 'CODE_MISMATCH') {
           String message = element.detail.toString();
-          Constants.showDialog_OkayOption(context, "Login Failed!", message);
+          Constants.showDialog_OkayOption(context, "Sign up Failed!", message);
           log.info("CODE_MISMATCH error handling complete.");
         }
       });
@@ -107,8 +200,11 @@ class SignUpConfirmationState extends State<SignUpConfirmation> {
     try {
       ResendSignUpCodeResult res = await Amplify.Auth.resendSignUpCode(username: data.name);
       log.info("Resend sign up code Successful!");
-      Constants.showDialog_OkayOption(context, "New Confirmation Code", "A new confirmation code has be sent to the "
-          "email associated with the account. Please enter code to verify account.");
+      Constants.showDialog_OkayOption(
+          context,
+          "New Confirmation Code",
+          "A new confirmation code has be sent to the "
+              "email associated with the account. Please enter code to verify account.");
       setState(() {
         enteredCode = "";
       });
@@ -157,178 +253,178 @@ class SignUpConfirmationState extends State<SignUpConfirmation> {
   Widget build(BuildContext context) {
     log.info("build(BuildContext context) called.");
 
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        /*leading: GestureDetector(
-          onTap: (){
-            Navigator.pop(context);
-          },
-          child: Icon(
-            Icons.arrow_back,
-            size: 30,
-            color: Colors.black,
-          ),
-        ),*/
-        title: Text(
-          "Sign Up Confirmation",
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-          ),
-        ),
+    final snackBar = SnackBar(
+      content: Text("DEBUG: Back button is disabled."),
+      /*action: SnackBarAction(
+        label: "Undo",
+        onPressed: () {
+          log.info("SnackBar pressed.");
+        },
+      ),*/
+    );
+
+    return WillPopScope(
+      onWillPop: () {
+        //Scaffold.of(context).showSnackBar(snackBar);
+        return Future.value(false);
+      },
+      child: Scaffold(
         backgroundColor: Colors.white,
-        elevation: 0,
-        centerTitle: true,
-        textTheme: Theme.of(context).textTheme,
-      ),
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Expanded(
-              child: Container(
-                color: Colors.white,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.symmetric(vertical: 7),
-                      child: Text(
-                        "Please check your email for code",
-                        style: TextStyle(
-                          fontWeight: FontWeight.normal,
-                          fontSize: 22,
-                          color: Color(0xFF818181),
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 2,
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          buildCodeNumberBox(enteredCode.length > 0 ? enteredCode.substring(0, 1) : "", context),
-                          buildCodeNumberBox(enteredCode.length > 1 ? enteredCode.substring(1, 2) : "", context),
-                          buildCodeNumberBox(enteredCode.length > 2 ? enteredCode.substring(2, 3) : "", context),
-                          buildCodeNumberBox(enteredCode.length > 3 ? enteredCode.substring(3, 4) : "", context),
-                          buildCodeNumberBox(enteredCode.length > 4 ? enteredCode.substring(4, 5) : "", context),
-                          buildCodeNumberBox(enteredCode.length > 5 ? enteredCode.substring(5, 6) : "", context),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(vertical: 14),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Text(
-                            "Didn't recieve code? ",
-                            style: TextStyle(
-                              fontSize: 18,
-                              color: Color(0xFF818181),
-                            ),
-                          ),
-                          SizedBox(
-                            width: 8,
-                          ),
-                          GestureDetector(
-                            onTap: () {
-                              log.info("\"Request new code\" button pressed.");
-                              resendCode(context);
-                            },
-                            child: Text(
-                              "Request again",
-                              style: TextStyle(
-                                fontSize: 18,
-                                color: MyAppColors.bluegreen1,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+        appBar: AppBar(
+          /*leading: GestureDetector(
+            onTap: (){
+              Navigator.pop(context);
+            },
+            child: Icon(
+              Icons.arrow_back,
+              size: 30,
+              color: Colors.black,
             ),
-            Container(
-              height: MediaQuery.of(context).size.height * 0.13,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(Radius.circular(25.0)),
-              ),
-              child: Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: GestureDetector(
-                        onTap: () {
-                          log.info("\"Confirm Sign Up\" button pressed.");
-                          confirmSignUp(context);
-                        },
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: MyAppColors.blue2,
-                            borderRadius: BorderRadius.all(Radius.circular(15)),
-                          ),
-                          child: Center(
-                            child: Text(
-                              "Confirm Sign Up",
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
+          ),*/
+          title: Text(
+            "Sign Up Confirmation",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+          backgroundColor: Colors.white,
+          elevation: 0,
+          centerTitle: true,
+          textTheme: Theme.of(context).textTheme,
+        ),
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  color: Colors.white,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 7),
+                        child: Text(
+                          "Please check your email for code",
+                          style: TextStyle(
+                            fontWeight: FontWeight.normal,
+                            fontSize: 22,
+                            color: Color(0xFF818181),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                      Expanded(
+                        flex: 2,
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            buildCodeNumberBox(enteredCode.length > 0 ? enteredCode.substring(0, 1) : "", context),
+                            buildCodeNumberBox(enteredCode.length > 1 ? enteredCode.substring(1, 2) : "", context),
+                            buildCodeNumberBox(enteredCode.length > 2 ? enteredCode.substring(2, 3) : "", context),
+                            buildCodeNumberBox(enteredCode.length > 3 ? enteredCode.substring(3, 4) : "", context),
+                            buildCodeNumberBox(enteredCode.length > 4 ? enteredCode.substring(4, 5) : "", context),
+                            buildCodeNumberBox(enteredCode.length > 5 ? enteredCode.substring(5, 6) : "", context),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 14),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              "Didn't recieve code? ",
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Color(0xFF818181),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 8,
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                log.info("\"Request new code\" button pressed.");
+                                resendCode(context);
+                              },
+                              child: Text(
+                                "Request again",
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: MyAppColors.bluegreen1,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            NumberPad(
-              onNumberSelected: (value) {
-                log.info("Number selected: " + value.toString());
-                setState(() {
-                  if(value != -1) {
-                    if(enteredCode.length < uniqueCodeLength){
-                      enteredCode = enteredCode + value.toString();
+              Container(
+                height: MediaQuery.of(context).size.height * 0.13,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: () {
+                            log.info("\"Confirm Sign Up\" button pressed.");
+                            confirmSignUp(context);
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: MyAppColors.blue2,
+                              borderRadius: BorderRadius.all(Radius.circular(15)),
+                            ),
+                            child: Center(
+                              child: Text(
+                                "Confirm Sign Up",
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              NumberPad(
+                onNumberSelected: (value) {
+                  log.info("Number selected: " + value.toString());
+                  setState(() {
+                    if (value != -1) {
+                      if (enteredCode.length < uniqueCodeLength) {
+                        enteredCode = enteredCode + value.toString();
+                      } else {
+                        log.warn("Max entered code length reached.");
+                      }
+                    } else if (enteredCode.length == 0) {
+                      log.warn("enteredCode length is already 0 and cannot be reduced anymore.");
                     } else {
-                      log.warn("Max entered code length reached.");
+                      enteredCode = enteredCode.substring(0, enteredCode.length - 1);
                     }
-                  } else if(enteredCode.length == 0) {
-                    log.warn("enteredCode length is already 0 and cannot be reduced anymore.");
-                  }
-                  else {
-                    enteredCode = enteredCode.substring(0, enteredCode.length - 1);
-                  }
-                  log.info("enteredCode= " + enteredCode);
-                });
-              },
-            ),
-          ],
+                    log.info("enteredCode= " + enteredCode);
+                  });
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
